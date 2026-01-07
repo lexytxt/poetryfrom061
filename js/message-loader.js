565d51ebd3816d1ed1b054111fc664364156e33e
lexy <mallexsh@gmail.com>
2026-01-07 22:21:39 +0200

    Update message-loader.js

--- js/message-loader.js
+++ js/message-loader.js
@@ -2,10 +2,16 @@
     const params = new URLSearchParams(window.location.search);
     const file = params.get('file');
     if (!file) return;
+
     const contentContainer = document.getElementById('message-content');
-    const markdownPath = 'messages/' + file;
+    const repoBase = location.pathname.replace(/\/[^/]*$/, '/');
+    const markdownPath = repoBase + 'messages/' + file;
+
     fetch(markdownPath)
-        .then(res => res.text())
+        .then(res => {
+            if (!res.ok) throw new Error('File not found');
+            return res.text();
+        })
         .then(md => {
             const lines = md.split('\n');
             let titleLineIndex = lines.findIndex(l => l.trim().startsWith('# '));
@@ -17,6 +23,7 @@
             } else {
                 titleText = file.replace('.md', '');
             }
+
             contentContainer.innerHTML =
                 `<h1>${titleText}</h1>` + marked.parse(lines.join('\n'));
         })
